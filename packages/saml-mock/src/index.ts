5dc1217984c32aed90bbaf1ea1761aae280e575d
Petri Ämmälä <petri.ammala@reaktor.com>
2026-04-10 14:49:31 +0300

    Change encryption algorithm to something more secure
    This is a mock implementation. Encryption is not known to be used anywhere.

--- packages/saml-mock/src/index.ts
+++ packages/saml-mock/src/index.ts
@@ -189,7 +189,7 @@ function encryptXml(xml: string, localName: string, encryptedLocalName: string):
       {
         rsa_pub: publicKey,
         pem: certificate,
-        encryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#aes256-cbc',
+        encryptionAlgorithm: 'http://www.w3.org/2009/xmlenc11#aes256-gcm',
         keyEncryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p'
       },
       (err, encryptedData) => {
